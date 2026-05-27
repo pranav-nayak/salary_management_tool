81e8df5c3137317b600308d6a7b7a781c2acf75e
pranav-nayak <prasannabr199595@gmail.com>
2026-05-27 17:48:04 +0530

    Add employee model specs

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Employee, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { build(:employee) }
+  it { should validate_presence_of(:first_name) }
+  it { should validate_presence_of(:last_name) }
+  it { should validate_presence_of(:email) }
+  it do
+    should validate_uniqueness_of(:email)
+  end
+  it { should validate_presence_of(:salary) }
+  it do
+    should validate_numericality_of(:salary)
+      .is_greater_than(0)
+  end
+
+  describe "#full_name" do
+    it "returns combined first and last name" do
+      employee = build(
+        :employee,
+        first_name: "John",
+        last_name: "Doe"
+      )
+
+      expect(employee.full_name)
+        .to eq("John Doe")
+    end
+  end
 end
