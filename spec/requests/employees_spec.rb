e2324e063bd1178d21ca7ba500b7e4570cc27eb8
pranav-nayak <prasannabr199595@gmail.com>
2026-05-27 17:54:08 +0530

    Add employee index endpoint

--- spec/requests/employees_spec.rb
+++ spec/requests/employees_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "Employees", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "GET /employees" do
+    it "returns successful response" do
+      get employees_path
+
+      expect(response)
+        .to have_http_status(:success)
+    end
   end
 end
