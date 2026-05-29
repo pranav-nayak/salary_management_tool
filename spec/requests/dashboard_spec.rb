a3771326ac0cffadbca6d9b79d3e1aba55360aa0
pranav-nayak <prasannabr199595@gmail.com>
2026-05-29 13:10:13 +0530

    Add dashboard spec and view template

--- spec/requests/dashboard_spec.rb
+++ spec/requests/dashboard_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "Dashboards", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/dashboard/index"
-      expect(response).to have_http_status(:success)
+  describe "GET /dashboard" do
+    it "renders dashboard successfully" do
+      get root_path
+
+      expect(response)
+        .to have_http_status(:success)
     end
   end
-
 end
